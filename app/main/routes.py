07fdac556ae876db1654d8c970c4a1a8cbb77150
Antonio Vilchez <antoniovmonge@gmail.com>
2021-10-26 19:09:45 +0200

    corrected feed rendering

--- app/main/routes.py
+++ app/main/routes.py
@@ -31,7 +31,7 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template("home.html", title='Feed', posts=posts.items,
+    return render_template("home.html", title='Feed', posts=posts,
                             next_url=next_url, prev_url=prev_url)
 
 
