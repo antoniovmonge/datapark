c7966e71d2c3cdd879e7036b0c34133f877860b3
Antonio Vilchez <antoniovmonge@gmail.com>
2021-10-26 09:20:35 +0200

    solution to text limit

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -5,7 +5,6 @@ from flask_pagedown.fields import PageDownField
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    body = PageDownField('Body', validators=[
-        DataRequired(), Length(min=1, max=500)])
+    body = PageDownField('Body', validators=[DataRequired()])
     submit = SubmitField('Submit')
     
