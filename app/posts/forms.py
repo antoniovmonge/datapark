f072ec4d64f7f0573931fe1951e0d64ef8bcf1d4
Antonio Vilchez <antoniovmonge@gmail.com>
2021-10-26 08:47:43 +0200

    string field

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from flask_pagedown.fields import PageDownField
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    body = PageDownField('Say something', validators=[DataRequired()])
+    body = PageDownField('Body', validators=[
+        DataRequired(), Length(min=1, max=500)])
     submit = SubmitField('Submit')
     
