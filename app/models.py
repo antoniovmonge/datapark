73fb61ae104b1b79f3cc578ca8dbfea3705b0cce
Antonio Vilchez <antoniovmonge@gmail.com>
2021-10-26 09:42:00 +0200

    subtitle

--- app/models.py
+++ app/models.py
@@ -82,6 +82,7 @@ class User(UserMixin, db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
+    subtitle = db.Column(db.String(100))
     body = db.Column(db.Text)
     body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
