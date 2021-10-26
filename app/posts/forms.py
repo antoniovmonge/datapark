6f11b78c9041de1d7616b1a3f313fdeaa35c30fa
Antonio Vilchez <antoniovmonge@gmail.com>
2021-10-26 08:13:06 +0200

    form body not length

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired
 from flask_pagedown.fields import PageDownField
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    body = PageDownField('Say something', validators=[
-        DataRequired(), Length(min=1, max=450)])
+    body = PageDownField('Say something', validators=[DataRequired()])
     submit = SubmitField('Submit')
     
